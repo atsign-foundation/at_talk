a465fa5e8580c31bbb904b28466a845a089da016
colin constable <colin@constable.cc>
2022-07-13 13:33:54 -0700

    Initial commit

---
+++ bin/at_talk.dart
@@ -0,0 +1,5 @@
+import 'package:at_talk/at_talk.dart' as at_talk;
+
+void main(List<String> arguments) {
+  print('Hello world: ${at_talk.calculate()}!');
+}
---
+++ test/at_talk_test.dart
@@ -0,0 +1,8 @@
+import 'package:at_talk/at_talk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
