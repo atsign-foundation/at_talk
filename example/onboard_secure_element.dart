e70ebd3d25ca641a7a5d4908d6b0f8b8561ef14e
murali-shris <murali.25@gmail.com>
2023-03-14 21:01:35 +0530

    test: snooker10 test

--- example/onboard_secure_element.dart
+++ example/onboard_secure_element.dart
@@ -3,14 +3,14 @@ import 'package:at_chops/at_chops.dart';
 import 'package:at_talk/src/iot/at_chops_secure_element.dart';
 
 Future<void> main() async {
-  final atSign = '@barbara';
+  final atSign = '@snooker10';
   AtOnboardingPreference atOnboardingConfig = AtOnboardingPreference()
     ..hiveStoragePath = 'storage/hive'
     ..namespace = 'wavi'
     ..downloadPath = 'storage/files'
     ..isLocalStoreRequired = true
     ..commitLogPath = 'storage/commitLog'
-    ..rootDomain = 'vip.ve.atsign.zone'
+    ..rootDomain = 'root.atsign.wtf'
     ..fetchOfflineNotifications = true
     //..atKeysFilePath = 'storage/files/barbara.atKeys'
     ..useAtChops = true
@@ -18,7 +18,7 @@ Future<void> main() async {
     ..hashingAlgoType=HashingAlgoType.sha256
     ..authMode=PkamAuthMode.sim
     ..publicKeyId='303037'
-  ..cramSecret='b43edaa255f738c763c2c79df4d49a4173b02c132e164b77650383fffe16f045ef8065c2ec05df8f4ae3475e04e83c7c99e96f52de9c4d1a915b67d24f590c99';
+  ..cramSecret='41bb12d6db2e49aa8942c9601f52fbf82e6809994155e161c099167808f61d3513073a87b64fa625a2918e505aacd54eb3f29ea29b80565077668ba49be8b892';
   AtOnboardingService onboardingService = AtOnboardingServiceImpl(atSign, atOnboardingConfig);
   // create empty keys in atchops. Encryption key pair will be set later on after generation
   onboardingService.atChops = AtChopsSecureElement(AtChopsKeys.create(null, null));
