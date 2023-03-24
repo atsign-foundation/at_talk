e3836f6ffd40366824aec1c5f8d616ade9539342
murali-shris <murali.25@gmail.com>
2023-03-24 10:33:44 +0530

    feat: changed print to logger in external signer

--- example/onboard_secure_element.dart
+++ example/onboard_secure_element.dart
@@ -1,9 +1,11 @@
 import 'package:at_onboarding_cli/at_onboarding_cli.dart';
 import 'package:at_chops/at_chops.dart';
 import 'package:at_talk/src/iot/at_chops_secure_element.dart';
+import 'package:at_utils/at_logger.dart';
 
-Future<void> main() async {
-  final atSign = '@snooker10';
+/// Usage: dart main.dart <cram_secret>
+Future<void> main(List<String> args) async {
+  final atSign = '@56hurt89';
   AtOnboardingPreference atOnboardingConfig = AtOnboardingPreference()
     ..hiveStoragePath = 'storage/hive'
     ..namespace = 'wavi'
@@ -12,15 +14,20 @@ Future<void> main() async {
     ..commitLogPath = 'storage/commitLog'
     ..rootDomain = 'root.atsign.wtf'
     ..fetchOfflineNotifications = true
-    //..atKeysFilePath = 'storage/files/barbara.atKeys'
+    ..atKeysFilePath = 'storage/files/@56hurt89_key.atKeys'
     ..useAtChops = true
     ..signingAlgoType=SigningAlgoType.ecc_secp256r1
     ..hashingAlgoType=HashingAlgoType.sha256
     ..authMode=PkamAuthMode.sim
     ..publicKeyId='303037'
-  ..cramSecret='41bb12d6db2e49aa8942c9601f52fbf82e6809994155e161c099167808f61d3513073a87b64fa625a2918e505aacd54eb3f29ea29b80565077668ba49be8b892';
+  ..cramSecret=args[0]
+  ..skipSync=true;
+  var logger = AtSignLogger('OnboardSecureElement');
+  logger.level = 'INFO';
+
   AtOnboardingService onboardingService = AtOnboardingServiceImpl(atSign, atOnboardingConfig);
   // create empty keys in atchops. Encryption key pair will be set later on after generation
   onboardingService.atChops = AtChopsSecureElement(AtChopsKeys.create(null, null));
   await onboardingService.onboard();
+  await onboardingService.authenticate();
 }
--- lib/src/iot/external_signer.dart
+++ lib/src/iot/external_signer.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
 import 'package:dart_periphery/dart_periphery.dart';
 
+import 'package:at_utils/at_logger.dart';
+
 //#TODO better name for this class
 class ExternalSigner {
   late Serial _serialPort;
   late String _customLibraryLocation;
   late String _applicationId;
   late String _privateKeyId;
+  late AtSignLogger _logger;
   ExternalSigner();
 
   void init() {
@@ -15,6 +18,7 @@ class ExternalSigner {
     _serialPort = Serial('/dev/ttyS0', Baudrate.b115200);
     _applicationId = 'A0000005590010';
     _privateKeyId = '303036';
+    _logger = AtSignLogger('ExternalSigner');
     //_publicKeyId = '303037';
   }
 
@@ -27,13 +31,13 @@ class ExternalSigner {
       _selectIOTSafeApplication(channelNumber!);
       return _readPublicKey(_serialPort, channelNumber, publicKeyId);
     } on Exception catch (e, trace) {
-      print('exception during signing ${e.toString()}');
-      print(trace);
+      _logger.severe('exception during signing ${e.toString()}');
+      _logger.severe(trace);
       throw e;
     } finally {
       if (channelNumber != null &&
           channelNumber.startsWith(RegExp(r'01|02|03'))) {
-        print('closing channel $channelNumber');
+        _logger.finest('closing channel $channelNumber');
         _closeChannel(_serialPort, channelNumber);
       }
       _serialPort.setVMIN(0);
@@ -46,25 +50,28 @@ class ExternalSigner {
     try {
       // Step 1. Open a logical channel. This should return a logical port  number [01|02|03] followed by [9000]. 9000 is success code.
       channelNumber = _openLogicalChannel();
+      _logger.info('opened logical channel #:$channelNumber');
       // Step 2. Select IOTsafe by application id
       _selectIOTSafeApplication(channelNumber!);
+
+      _logger.info('selected IOTSafe application');
 
       // Step 3. Compute signature init
       _computeSignatureInit(channelNumber);
 
       // Step 4. Compute signature update
       _computeSignatureUpdate(channelNumber, dataHash.toUpperCase());
-
-      // Strp 5. Retrieve computed signature
+      _logger.info('signature computed. Retrieving the signature');
+      // Step 5. Retrieve computed signature
       var signature = _retrieveSignature(channelNumber);
       return signature.toLowerCase();
     } on Exception catch (e, trace) {
-      print('exception during signing ${e.toString()}');
-      print(trace);
+      _logger.severe('exception during signing ${e.toString()}');
+      _logger.severe(trace);
     } finally {
       if (channelNumber != null &&
           channelNumber.startsWith(RegExp(r'01|02|03'))) {
-        print('closing channel $channelNumber');
+        _logger.finest('closing channel $channelNumber');
         _closeChannel(_serialPort, channelNumber);
       }
       _serialPort.setVMIN(0);
@@ -84,10 +91,11 @@ class ExternalSigner {
     _serialPort.writeString(
         "AT+CSIM=20, \"8${channelNumber.substring(1)}CD0000058503${publicKeyId}\"\r\n");
     var readPublicKeyResult = _serialPort.read(256, 1000);
-    print('readPublicKeyResult :$readPublicKeyResult');
+    _logger.finest('readPublicKeyResult :$readPublicKeyResult');
     bool isReadPublicKeyResultSuccess =
         _parseResult(readPublicKeyResult.toString(), ATCommand.readPublicKey);
-    print('isReadPublicKeyResultSuccess $isReadPublicKeyResultSuccess');
+    _logger
+        .finest('isReadPublicKeyResultSuccess $isReadPublicKeyResultSuccess');
     if (!isReadPublicKeyResultSuccess) {
       throw Exception('Read public key failure');
     }
@@ -99,13 +107,13 @@ class ExternalSigner {
     if (publicKey == null) {
       throw Exception('cannot read public key');
     }
-    print('retrievePublicKeyResult :$publicKey');
+    _logger.finest('retrievePublicKeyResult :$publicKey');
     return publicKey;
   }
 
   String? _parsePublicKeyResult(AtCsimResult csimResult) {
     var commandResult = csimResult.result;
-    print(commandResult);
+    _logger.finest(commandResult);
     if (commandResult != null && commandResult.startsWith('344549438641')) {
       commandResult = commandResult.substring(12, commandResult.length - 4);
       return commandResult;
@@ -116,7 +124,7 @@ class ExternalSigner {
   String? _openLogicalChannel() {
     final openChannelResult = _getChannelNumber(_openChannel(_serialPort));
     var channelNumber = openChannelResult.channelNumber;
-    print('channelNumber:$channelNumber');
+    _logger.finest('channelNumber:$channelNumber');
     if (openChannelResult.success) {
       return channelNumber;
     } else {
@@ -125,11 +133,11 @@ class ExternalSigner {
   }
 
   String _openChannel(Serial serialPort) {
-    print('Opening a non-default logical channel');
+    _logger.finest('Opening a non-default logical channel');
     serialPort.writeString('AT+CSIM=10, \"0070000000\"\r\n');
     var event = serialPort.read(256, 1000);
     final result = event.toString();
-    print('openChannelResult: $result');
+    _logger.finest('openChannelResult: $result');
     return result;
   }
 
@@ -137,10 +145,10 @@ class ExternalSigner {
     _serialPort.writeString(
         "AT+CSIM=24, \"${channelNumber}A4040007${_applicationId}\"\r\n");
     var selectApplicationResult = _serialPort.read(256, 1000);
-    print('selectApplicationResult :$selectApplicationResult');
+    _logger.finest('selectApplicationResult :$selectApplicationResult');
     bool isValidApplication =
         _parseResult(selectApplicationResult.toString(), ATCommand.selectApp);
-    print('isValidApplication $isValidApplication');
+    _logger.finest('isValidApplication $isValidApplication');
   }
 
   void _computeSignatureInit(String channelNumber) {
@@ -156,10 +164,11 @@ class ExternalSigner {
     _serialPort.writeString(
         "AT+CSIM=40,\"8${channelNumber.substring(1)}2A00010F8403${_privateKeyId}A1010391020001920104\"\r\n");
     var computeSignatureInitResult = _serialPort.read(256, 1000);
-    print('computeSignatureInitResult :$computeSignatureInitResult');
+    _logger.info('computeSignatureInitResult :$computeSignatureInitResult');
     bool isComputeSignatureInitSuccess = _parseResult(
         computeSignatureInitResult.toString(), ATCommand.computeSignatureInit);
-    print('isComputeSignatureInitSuccess: $isComputeSignatureInitSuccess');
+    _logger.finest(
+        'isComputeSignatureInitSuccess: $isComputeSignatureInitSuccess');
   }
 
   void _computeSignatureUpdate(String channelNumber, String dataHash) {
@@ -169,7 +178,7 @@ class ExternalSigner {
     // 22 - length of command
     // 9E - tag for hash
     // 20 -length of hash
-    print('computing signature from external hash');
+    _logger.finest('computing signature from external hash');
     _serialPort.writeString(
         "AT+CSIM=78,\"8${channelNumber.substring(1)}2B8001229E20$dataHash\"\r\n");
     var computeSignatureUpdateResult = '';
@@ -178,26 +187,26 @@ class ExternalSigner {
       final readEvent = _serialPort.read(512, 1000);
       computeSignatureUpdateResult += readEvent.toString();
       if (!readEvent.toString().contains('OK')) {
-        print('Got result: $computeSignatureUpdateResult');
+        _logger.finest('Got result: $computeSignatureUpdateResult');
         sleep(Duration(seconds: 2));
         continue;
       }
       break;
     }
-    print(
+    _logger.finest(
         'computeSignatureUpdateResult ${computeSignatureUpdateResult.toString()}');
     bool isComputeSignatureSuccess = _parseComputeSignatureUpdateResult(
         computeSignatureUpdateResult.toString());
-    print('isComputeSignatureSuccess :$isComputeSignatureSuccess');
+    _logger.finest('isComputeSignatureSuccess :$isComputeSignatureSuccess');
   }
 
   String _retrieveSignature(String channelNumber) {
     _serialPort.writeString(
         "AT+CSIM=10,\"8${channelNumber.substring(1)}C0000043\"\r\n");
     var getSignatureResult = _serialPort.read(256, 1000);
-    print('getSignatureResult $getSignatureResult');
+    _logger.finest('getSignatureResult $getSignatureResult');
     final signatureData = _parseAtCsimResult(getSignatureResult.toString());
-    print('signature: ${signatureData.result}');
+    _logger.info('signature: ${signatureData.result}');
     var signatureStr = '';
     if (signatureData.result != null &&
         signatureData.result!.startsWith('330040')) {
@@ -213,84 +222,89 @@ class ExternalSigner {
     if (atCsimResult.result == null ||
         atCsimResult.result!.isEmpty ||
         atCsimResult.success == false) {
-      print('unexpected response from open channel: $openChannelResult');
+      _logger
+          .finest('unexpected response from open channel: $openChannelResult');
       return OpenChannelResult(false, null);
     }
     if (atCsimResult.result!.startsWith(RegExp(r'01|02|03'))) {
       if (atCsimResult.result!.substring(2, 6) == '9000') {
         final channelNumber = atCsimResult.result!.substring(0, 2);
-        print('open channel successful');
+        _logger.finest('open channel successful');
         return OpenChannelResult(true, channelNumber);
       } else {
-        print(
+        _logger.finest(
             'open channel failed with non-success code: ${atCsimResult.result!.substring(2, 6)}');
       }
     } else {
-      print(
+      _logger.finest(
           'open channel failed.Returned channel number : ${atCsimResult.result!.substring(0, 2)}');
       return OpenChannelResult(false, atCsimResult.result!.substring(0, 2));
     }
     return OpenChannelResult(false, null);
   }
-}
 
-bool _parseResult(String result, ATCommand atCommand) {
-  final atCsimResult = _parseAtCsimResult(result);
-  if (atCsimResult.result == null ||
-      atCsimResult.result!.isEmpty ||
-      atCsimResult.success == false) {
-    print('unexpected response from ${atCommand.toString()}: $atCsimResult');
+  bool _parseResult(String result, ATCommand atCommand) {
+    final atCsimResult = _parseAtCsimResult(result);
+    if (atCsimResult.result == null ||
+        atCsimResult.result!.isEmpty ||
+        atCsimResult.success == false) {
+      _logger.finest(
+          'unexpected response from ${atCommand.toString()}: $atCsimResult');
+      return false;
+    }
+    if (atCommand == ATCommand.readPublicKey && atCsimResult.result == '6147') {
+      return true;
+    } else if (atCsimResult.result == '9000') {
+      return true;
+    } else {
+      _logger.finest(
+          'failure code in ${atCommand.toString()} ${atCsimResult.result}');
+    }
     return false;
   }
-  if (atCommand == ATCommand.readPublicKey && atCsimResult.result == '6147') {
-    return true;
-  } else if (atCsimResult.result == '9000') {
-    return true;
-  } else {
-    print('failure code in ${atCommand.toString()} ${atCsimResult.result}');
-  }
-  return false;
-}
 
-bool _parseComputeSignatureUpdateResult(String result) {
-  final atCsimResult = _parseAtCsimResult(result);
-  if (atCsimResult.result == null ||
-      atCsimResult.result!.isEmpty ||
-      atCsimResult.success == false) {
-    print('unexpected response from compute signature update: $atCsimResult');
+  bool _parseComputeSignatureUpdateResult(String result) {
+    final atCsimResult = _parseAtCsimResult(result);
+    if (atCsimResult.result == null ||
+        atCsimResult.result!.isEmpty ||
+        atCsimResult.success == false) {
+      _logger.finest(
+          'unexpected response from compute signature update: $atCsimResult');
+      return false;
+    }
+    if (atCsimResult.result == '6143') {
+      return true;
+    }
     return false;
   }
-  if (atCsimResult.result == '6143') {
-    return true;
-  }
-  return false;
-}
 
-AtCsimResult _parseAtCsimResult(String result) {
-  final atCsimResult = AtCsimResult();
-  if (result.isEmpty || !result.contains(AtCsimResult.pattern)) {
-    print('unexpected response : $result');
+  AtCsimResult _parseAtCsimResult(String result) {
+    final atCsimResult = AtCsimResult();
+    if (result.isEmpty || !result.contains(AtCsimResult.pattern)) {
+      _logger.finest('unexpected response : $result');
+      return atCsimResult;
+    }
+    int startIndex = result.indexOf(AtCsimResult.pattern);
+    int bytesStartIndex = startIndex + AtCsimResult.pattern.length;
+    int bytesEndindex =
+        startIndex + result.substring(startIndex).indexOf(',\"');
+    String bytesToRead = result.substring(bytesStartIndex, bytesEndindex);
+    atCsimResult.bytesToRead = int.parse(bytesToRead);
+    atCsimResult.result = result.substring(
+        bytesEndindex + 2, bytesEndindex + 2 + atCsimResult.bytesToRead);
+    _logger.finest('atCsimResult.result ${atCsimResult.result}');
+    atCsimResult.success = true;
     return atCsimResult;
   }
-  int startIndex = result.indexOf(AtCsimResult.pattern);
-  int bytesStartIndex = startIndex + AtCsimResult.pattern.length;
-  int bytesEndindex = startIndex + result.substring(startIndex).indexOf(',\"');
-  String bytesToRead = result.substring(bytesStartIndex, bytesEndindex);
-  atCsimResult.bytesToRead = int.parse(bytesToRead);
-  atCsimResult.result = result.substring(
-      bytesEndindex + 2, bytesEndindex + 2 + atCsimResult.bytesToRead);
-  print('atCsimResult.result ${atCsimResult.result}');
-  atCsimResult.success = true;
-  return atCsimResult;
-}
 
-void _closeChannel(Serial port, String channelNumber) {
-  print('closing channel : $channelNumber');
-  final channelCloseCommand = 'AT+CSIM=10, \"007080${channelNumber}00\"\r\n';
-  print('channelCloseCommand: $channelCloseCommand');
-  port.writeString(channelCloseCommand);
-  final result = port.read(256, 1000);
-  print('close channel result: ${result.toString()}');
+  void _closeChannel(Serial port, String channelNumber) {
+    _logger.finest('closing channel : $channelNumber');
+    final channelCloseCommand = 'AT+CSIM=10, \"007080${channelNumber}00\"\r\n';
+    _logger.finest('channelCloseCommand: $channelCloseCommand');
+    port.writeString(channelCloseCommand);
+    final result = port.read(256, 1000);
+    _logger.finest('close channel result: ${result.toString()}');
+  }
 }
 
 class OpenChannelResult {
